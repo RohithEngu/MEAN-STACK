c60c691376026eba3faa677c096102b47936772f
Rohith Engu <engu.rohit@gmail.com>
2016-07-02 22:55:38 -0400

    ver 2.0

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -25,6 +25,26 @@ app.config([
 					return posts.get($stateParams.id);
 				}]
 			}
+		})
+		.state('login',{
+			url: '/login',
+			templateUrl: '/login.html',
+			controller: 'AuthCtrl',
+			onEnter: ['$state', 'auth', function($state,auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
+		})
+		.state('register',{
+			url: '/register',
+			templateUrl: '/register.html',
+			controller: 'AuthCtrl',
+			onEnter: ['$state', 'auth', function($state, auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
 		});
 
 		$urlRouterProvider.otherwise('home');
@@ -34,9 +54,10 @@ app.config([
 app.controller('MainCtrl',[
 	'$scope',
 	'posts',
-	function($scope,posts){
+	'auth',
+	function($scope,posts, auth){
 		$scope.posts = posts.posts;
-
+		$scope.isLoggedIn = auth.isLoggedIn;
 		$scope.addPost = function(){
 			if(!$scope.title || $scope.title === '') {return;}
 			posts.create({
@@ -57,8 +78,10 @@ app.controller('PostsCtrl',[
 	'$scope',
 	'posts',
 	'post',
-	function($scope,posts,post){
+	'auth',
+	function($scope,posts,post, auth){
 		$scope.post = post;
+		$scope.isLoggedIn = auth.isLoggedIn;
 
 		$scope.addComment = function(){
 			if($scope.body === ''){return;}
@@ -77,9 +100,38 @@ app.controller('PostsCtrl',[
 		};
 
 	}]);
+app.controller('AuthCtrl', [
+	'$scope',
+	'$state',
+	'auth',
+	function($scope, $state, auth){
+	  $scope.user = {};
 
+	  $scope.register = function(){
+	    auth.register($scope.user).error(function(error){
+	      $scope.error = error;
+	    }).then(function(){
+	      $state.go('home');
+	    });
+	  };
 
-app.factory('posts',['$http', function($http){
+	  $scope.logIn = function(){
+	    auth.logIn($scope.user).error(function(error){
+	      $scope.error = error;
+	    }).then(function(){
+	      $state.go('home');
+	    });
+	  };
+	}]);
+app.controller('NavCtrl', [
+	'$scope',
+	'auth',
+	function($scope, auth){
+	  $scope.isLoggedIn = auth.isLoggedIn;
+	  $scope.currentUser = auth.currentUser;
+	  $scope.logOut = auth.logOut;
+	}]);
+app.factory('posts',['$http', 'auth', function($http, auth){
 	var o = {
 		posts: []
 	};
@@ -97,23 +149,31 @@ app.factory('posts',['$http', function($http){
 	};
 
 	o.upvote = function(post){
-		return $http.put('/posts/'+post._id+'/upvote').success(function(data){
+		return $http.put('/posts/'+post._id+'/upvote', null, {
+			headers: {Authorization: 'Bearer '+auth.getToken()}
+		}).success(function(data){
 			post.upvotes += 1;
 		});
 	};
 
 	o.create = function(post){
-		return $http.post('/posts',post).success(function(data){
+		return $http.post('/posts',post, {
+			headers: {Authorization: 'Bearer '+auth.getToken()}
+		}).success(function(data){
 			o.posts.push(data);
 		});
 	};
 
 	o.addComment = function(id,comment){
-		return $http.post('/posts/'+id+'/comments',comment);
+		return $http.post('/posts/'+id+'/comments',comment,{
+			headers: {Authorization: 'Bearer '+auth.getToken()}
+		});
 	};
 
 	o.upvoteComment = function(post,comment){
-		return $http.put('/posts/'+post._id+'/comments/'+comment._id+'/upvote').success(function(data){
+		return $http.put('/posts/'+post._id+'/comments/'+comment._id+'/upvote', null,{
+			headers: {Authorization: 'Bearer '+auth.getToken()}
+		}).success(function(data){
 			comment.upvotes += 1;
 		});
 	};
@@ -121,4 +181,46 @@ app.factory('posts',['$http', function($http){
 	return o;
 }]);
 
+app.factory('auth', ['$http','$window', '$rootScope',function($http, $window, $rootScope){
+	var auth = {
+	    saveToken: function (token){
+	      $window.localStorage['flapper-news-token'] = token;
+	    },
+	    getToken: function (){
+	      return $window.localStorage['flapper-news-token'];
+	    },
+	    isLoggedIn: function(){
+	      var token = auth.getToken();
 
+	      if(token){
+	        var payload = JSON.parse($window.atob(token.split('.')[1]));
+	        
+	        return payload.exp > Date.now() / 1000;
+	      } else {
+	        return false;
+	      }
+	    },
+	    currentUser: function(){
+	      if(auth.isLoggedIn()){
+	        var token = auth.getToken();
+	        var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+	        return payload.username;
+	      }
+	    },
+	    register: function(user){
+	      return $http.post('/register', user).success(function(data){
+	        auth.saveToken(data.token);
+	      });
+	    },
+	    logIn: function(user){
+	      return $http.post('/login', user).success(function(data){
+	        auth.saveToken(data.token);
+	      });
+	    },
+	    logOut: function(){
+	      $window.localStorage.removeItem('flapper-news-token');
+	    }
+	};
+	return auth;
+}]);
